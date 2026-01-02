daaab86a66c1967fb1140d8f5b2c7b2fbae9bc9a
HWZ-499 <146724503+HWZ-499@users.noreply.github.com>
2026-01-02 18:44:42 +0800

    Add debug log for index.tsx loading confirmation
    Added a debug log to confirm the entry file is loaded.

--- index.tsx
+++ index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Debug log to confirm the entry file is loaded in the browser
+console.log('[bloom] index.tsx loaded');
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
